fab7d75bf7b44933ecb2170dd3e3a54ad0a87be1
Abdulloh <lochin.tj@gmail.com>
2026-05-17 02:43:58 -0700

    style: simplify global styles and clean up shadcn theme variables

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,7 @@
 import { createRoot } from "react-dom/client";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-
-import "./index.css";
-import App from "./App.tsx";
+import "@/app/styles/index.css";
+import App from "@/app/App";
 
 const queryClient = new QueryClient();
 
